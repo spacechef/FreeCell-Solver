cdcdecbdd4b3c4f3a4c6f6f7f3c30a86e843263d
Bart <mail@bartsunter.nl>
2014-01-09 16:34:27 +0100

    voeg kleur functies toe aan kaart

--- Kaart.py
+++ Kaart.py
@@ -9,4 +9,15 @@ class Kaart(object):
     def __init__(self, nummer, soort):
         self.nummer = nummer
         self.soort = soort
-        
+
+    def geef_kleur(self):
+        if self.soort == "R" or self.soort == "H"
+            return "rood"
+        else
+            return "zwart"
+
+    def is_kleur(self, kleur):
+        if self.geef_kleur == kleur
+            return true
+        else
+            return false
