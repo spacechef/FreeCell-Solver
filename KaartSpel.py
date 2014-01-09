26105e52d1fbd203e3b75725e0bb7ec1ae31d44f
Bart <mail@bartsunter.nl>
2014-01-09 16:07:09 +0100

    add kaartspel class

---
+++ KaartSpel.py
@@ -0,0 +1,27 @@
+from Kaart import Kaart
+from random import shuffle
+
+class KaartSpel(object):
+
+    def __init__(self):
+        self.kaartspel = []
+
+    def maak(self):
+        for i in range(1,13):
+            kaart = Kaart(i, "R")
+            self.voeg_toe(kaart)
+        for i in range(1,13):
+            kaart = Kaart(i, "H")
+            self.voeg_toe(kaart)
+        for i in range(1,13):
+            kaart = Kaart(i, "S")
+            self.voeg_toe(kaart)
+        for i in range(1,13):
+            kaart = Kaart(i, "K")
+            self.voeg_toe(kaart)
+
+    def voeg_toe(self, kaart):
+        self.kaartspel.append(kaart)
+
+    def schud(self):
+        shuffle(self.kaartspel)
