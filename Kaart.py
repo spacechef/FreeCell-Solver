a06f36943dcd8f7fd906ecd8eb850c2a82acddb1
spacechef <dark_maniac@hotmail.com>
2014-01-09 16:35:47 +0100

    fix newbfouten

--- Kaart.py
+++ Kaart.py
@@ -11,13 +11,13 @@ class Kaart(object):
         self.soort = soort
 
     def geef_kleur(self):
-        if self.soort == "R" or self.soort == "H"
+        if self.soort == "R" or self.soort == "H":
             return "rood"
-        else
+        else:
             return "zwart"
 
     def is_kleur(self, kleur):
-        if self.geef_kleur == kleur
-            return true
-        else
-            return false
+        if self.geef_kleur == kleur:
+            return True
+        else:
+            return False
