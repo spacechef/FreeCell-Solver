8a760c4aa2197849020d5523abc71618d3cfa65b
spacechef <dark_maniac@hotmail.com>
2014-01-14 20:25:56 +0100

    beter

--- Bord.py
+++ Bord.py
@@ -22,12 +22,15 @@ class Bord(object):
     
     def maak_doel_en_freecellen(self):
         for aantal_freecells in range(self.aantal_freecells):
-            freecell = Kaart(0,'L')
+            freecell = Kolom()
             self.freecells.append(freecell)
+            freecell.voeg_toe(Kaart(0, 'L'))
         
         for aantal_doelcellen in range(self.aantal_doelcellen):
-            doelcel = Kaart(0, 'L')
+            doelcel = Kolom()
             self.doelcellen.append(doelcel)
+            kaart = Kaart(0, 'L')
+            doelcel.voeg_toe(kaart)
             
     def maak_kolommen(self):
         for aantal_kolommen in range(self.aantal_kolommen):
@@ -39,23 +42,23 @@ class Bord(object):
     
     def kaart_kan_naar_doelcel(self, kaart):
         for doelcel in self.doelcellen:
-            if kaart.nummer == doelcel.nummer + 1 and kaart.soort == doelcel.soort:
+            if kaart.nummer == doelcel.rij_met_kaarten[-1].nummer + 1 and kaart.soort == doelcel.rij_met_kaarten[-1].soort:
                 return True
-            elif kaart.nummer == doelcel.nummer + 1 and doelcel.soort == 'L':
+            elif kaart.nummer == doelcel.rij_met_kaarten[-1].nummer + 1 and doelcel.rij_met_kaarten[-1].soort == 'L':
                 return True
-    
+
     def kaart_kan_naar_freecel(self, kaart):
         for freecel in self.freecells:
-            if kaart.nummer == freecel.nummer + 1 and kaart.soort == freecel.soort:
+            if kaart.nummer == freecel.rij_met_kaarten[-1].nummer + 1 and kaart.soort == freecel.rij_met_kaarten[-1].soort:
                 return True
-            elif kaart.nummer == freecel.nummer + 1 and freecel.soort == 'L':
+            elif kaart.nummer == freecel.rij_met_kaarten[-1].nummer + 1 and freecel.rij_met_kaarten[-1].soort == 'L':
                 return True
     
     def voeg_toe_aan_doelcel(self, kaart):
         teller = 0
         for doelcel in self.doelcellen:
-            if doelcel.nummer == 0 or kaart.nummer == doelcel.nummer + 1 and kaart.soort == doelcel.soort: #hier moet soortcheck bij
-                self.doelcellen[teller] = kaart
+            if doelcel.rij_met_kaarten[-1].nummer == 0 or kaart.nummer == doelcel.rij_met_kaarten[-1].nummer + 1 and kaart.soort == doelcel.rij_met_kaarten[-1].soort: #hier moet soortcheck bij
+                doelcel.voeg_toe(kaart)
                 break
             else:
                 teller +=1
@@ -63,8 +66,8 @@ class Bord(object):
     def voeg_toe_aan_freecel(self, kaart):
         teller = 0
         for freecell in self.freecells:
-            if freecell.nummer == 0 or kaart.nummer == freecell.nummer + 1 and kaart.soort == freecell.soort: #hier moet soortcheck bij
-                self.freecells[teller] = kaart
+            if freecell.rij_met_kaarten[-1].nummer == 0 or kaart.nummer == freecell.rij_met_kaarten[-1].nummer + 1 and kaart.soort == freecell.rij_met_kaarten[-1].soort: #hier moet soortcheck bij
+                freecell.voeg_toe(kaart)
                 break
             else:
                 teller += 1
@@ -72,11 +75,11 @@ class Bord(object):
     def druk_af(self):
         print '{:^30}'.format('Freecells'),'|' '{:^30}'.format('Doelcellen')
         print 60*'-'
-        for kaart in self.freecells:
-            print '[{:2}{:1}] '.format(kaart.nummer, kaart.soort),
+        for kolom in self.freecells:
+            print '[{:2}{:1}] '.format(kolom.rij_met_kaarten[-1].nummer, kolom.rij_met_kaarten[-1].soort),
         print '{:^7}'.format('|'),
-        for kaart in self.doelcellen:
-            print '[{:2}{:1}] '.format(kaart.nummer, kaart.soort),
+        for kolom in self.doelcellen:
+            print '[{:2}{:1}] '.format(kolom.rij_met_kaarten[-1].nummer, kolom.rij_met_kaarten[-1].soort),
         print '\n' + 60*'-'
         mapped_kolommen = map(None, self.rij_met_kolommen[0].rij_met_kaarten, self.rij_met_kolommen[1].rij_met_kaarten, self.rij_met_kolommen[2].rij_met_kaarten,self.rij_met_kolommen[3].rij_met_kaarten,self.rij_met_kolommen[4].rij_met_kaarten,self.rij_met_kolommen[5].rij_met_kaarten, self.rij_met_kolommen[6].rij_met_kaarten, self.rij_met_kolommen[7].rij_met_kaarten)
         for kolom in mapped_kolommen:
--- __init__.py
+++ __init__.py
@@ -22,9 +22,6 @@ def deel_kaarten():
         bord.rij_met_kolommen[teller].voeg_toe(kaart)
         teller += 1
 
-def doe_zet():
-    bepaal_speelbare_kaarten()
-#     verplaats_speelbare_kaart_naar_doelcel()
 def bepaal_aantal_freecells():
     aantal_freecells = 0
     for kaart in bord.freecells:
@@ -32,72 +29,48 @@ def bepaal_aantal_freecells():
             aantal_freecells +=1
             
     return aantal_freecells
+
 def speel_kaart():
     aantal_freecells = bord.aantal_freecells
     
-    for kolom in bord.rij_met_kolommen:
-        speelbare_kaarten_in_kolom = kolom.rij_met_kaarten[-aantal_freecells:]
-        for kaart in speelbare_kaarten_in_kolom:
+    for kolom in bord.rij_met_kolommen: #de freecell checker klopt niet want hij kijkt niet of de kaart daadwerkelijk gespeeld kan worden dus blijft hij altijd tot 4 diepte kijken ipv of de kaat gespeeld kan worden
+        speelbare_kaarten = kolom.rij_met_kaarten[-aantal_freecells:] #deze moet in per beurt opgehoogd worden
+        
+        for kaart in speelbare_kaarten:
             if bord.kaart_kan_naar_doelcel(kaart):
                 bord.voeg_toe_aan_doelcel(kaart)
                 zet_kaarten_goed(kaart, kolom)
                 bord.druk_af()
+                freecel_naar_doelcel() #probeersel
             elif bord.kaart_kan_naar_freecel(kaart):
                 bord.voeg_toe_aan_freecel(kaart)
                 zet_kaarten_goed(kaart, kolom)
                 bord.druk_af()
-            
-            
-def bepaal_speelbare_kaarten():
-    aantal_freecells = bepaal_aantal_freecells()
+                freecel_naar_doelcel() #probeersel
+
+def freecel_naar_doelcel(): #probeersel
+    for freecel in bord.freecells:
+        for kaart in freecel:
+            if bord.kaart_kan_naar_doelcel(kaart):
+                bord.voeg_toe_aan_doelcel(kaart)
+                zet_kaarten_goed(kaart, freecel)
+                bord.druk_af()
+
     
-    for kolom in bord.rij_met_kolommen:
-        speelbare_kaarten_in_kolom = kolom.rij_met_kaarten[-aantal_freecells:]
-        for kaart in speelbare_kaarten_in_kolom:
-            for doelcel in bord.doelcellen: #dit moet denk ik in 4 aparte if'jes ivm overbodig vaak checken nu
-                if kaart.nummer == doelcel.nummer + 1 and kaart.soort == doelcel.soort: #hier moet nog kleurcheck bij en de freecell en doelcellen moeten kaartobjecten worden
-                    
-                    zet_kaarten_goed(kaart, kolom)
-                    print kaart.nummer, kaart.soort
-                    print kaart
-                    bord.voeg_toe_aan_doelcel(kaart)
-#                     kolom.verwijder_kaart(kaart)
-                    bord.druk_af()
-                    break
-                elif kaart.nummer == doelcel.nummer + 1 and doelcel.soort == 'L':
-                    zet_kaarten_goed(kaart, kolom)
-                    bord.voeg_toe_aan_doelcel(kaart)
-#                     print kaart.nummer, kaart.soort
-                    print kaart
-#                     kolom.verwijder_kaart(kaart)
-                    bord.druk_af()                                  
-#             if kaart.nummer == bord.doelcellen[0].nummer + 1 and kaart.soort == 
 def zet_kaarten_goed(kaart, kolom):
     index = kolom.rij_met_kaarten.index(kaart)
-    #aantal_bovenliggende_kaarten = len(kolom.rij_met_kaarten) - index
-    del kolom.rij_met_kaarten[index]
     for kaart in kolom.rij_met_kaarten:
         print kaart.nummer, kaart.soort
-    print index
+    print index    
+#     del kolom.rij_met_kaarten[index]
     for kaart in kolom.rij_met_kaarten: #haalt de laatste kaart nog niet weg
-        if kolom.rij_met_kaarten.index(kaart) >= index:
-           kolom.verwijder_kaart(kaart)
-
-#     for nummer, kaart in enumerate(kolom.rij_met_kaarten):
-#         if nummer > index:
-#             bord.voeg_toe_aan_freecel(kaart)
-#             kolom.verwijder_kaart(kaart)
-#     for aantal in range(1, aantal_bovenliggende_kaarten): #range is 1 tot aantal_bovenliggende kaarten anders speelt hij de gespeelde kaart nog een x (dubbel) naar de freecell
-#         bord.voeg_toe_aan_freecel(kolom.rij_met_kaarten[index + aantal]) #lelijke code nog een x naar kijken + hier gaat wat fout met index cijfers
-#         kolom.verwijder_kaart(kolom.rij_met_kaarten[index + aantal]) #hier ook iets fout
-                       
-            
-# def print_kolommen():
-#     for kolom in bord.rij_met_kolommen:
-#         for kaart in kolom.rij_met_kaarten:            
-#             print kaart,
-#         print 'aantal: %d'  %kolom.aantal
-
+        if kolom.rij_met_kaarten.index(kaart) > index: #index positie van bovenliggende kaart veranderd doordat er een entry verwijderd word
+            bord.voeg_toe_aan_freecel(kaart)
+            kolom.verwijder_kaart(kaart)
+          
 deel_kaarten()
 bord.druk_af()
-speel_kaart()
+
+levend = True
+while levend == True:
+    speel_kaart()
